8acb36cfd601083ea8923b3634f857de77368837
AJ Memmel <averyparanoiduser@gmail.com>
2023-01-27 19:53:35 -0600

    cleaning up move code

--- command/Move.java
+++ command/Move.java
@@ -21,12 +21,12 @@ public class Move extends CommandBase {
 	private final double[] distanceTotal; // {north, east, rotation} (signed)
 	private final int method; // 1 2 or 3 (#args to setVelocity/setRotation)
 
-	private double[] fast; // {north, east, rotation} (signed)
-	public double[] slow; // {north, east, rotation} (signed)
+	private double[] fast = new double[3]; // {north, east, rotation} (signed)
+	public double[] slow = new double[3]; // {north, east, rotation} (signed)
 	private AccelerationLimits acceleration; // like getAccelerationLimits, but scaled
 
-	private double[] distanceRemaining; // {north, east, rotation} (signed)
-	private boolean[] finished; // {north, east, rotation}
+	private double[] distanceRemaining = new double[3]; // {north, east, rotation} (signed)
+	private boolean[] finished = new boolean[3]; // {north, east, rotation}
 
 	private Position origin;
 
@@ -47,7 +47,7 @@ public class Move extends CommandBase {
 		this.chassis = hardware.getChassis();
 
 //		distanceTotal_ = new double[]{north, east * .68, rotation * 7 / 8}; //fudged values for the multipliers
-		distanceTotal = new double[]{north, east * .68, rotation * 7 / 8}; //fudged values for the multipliers
+		distanceTotal = new double[]{north, east, rotation}; //fudged values for the multipliers
 		this.method = method;
 	}
 
@@ -65,72 +65,73 @@ public class Move extends CommandBase {
 			Math.abs(distanceTotal[2]) / max.get(VelocityDirection.ROTATION)
 		};
 		double maxScale = Math.max(scale[0], Math.max(scale[1], scale[2]));
-		scale = new double[]{scale[0] / maxScale, scale[1] / maxScale, scale[2] / maxScale}; // 0 - 1
-		double[] sign = {
-			Math.signum(distanceTotal[0]),
-			Math.signum(distanceTotal[1]),
-			Math.signum(distanceTotal[2])
-		};
 
-		// scale speeds and acceleration/deceleration
-		fast = new double[]{
-			Math.max(min.get(VelocityDirection.NORTH), max.get(VelocityDirection.NORTH) * scale[0]) * sign[0],
-			Math.max(min.get(VelocityDirection.EAST), max.get(VelocityDirection.EAST) * scale[1]) * sign[1],
-			Math.max(min.get(VelocityDirection.ROTATION), max.get(VelocityDirection.ROTATION) * scale[2]) * sign[2]
-		}; // don't let maximum scale below minimum
-		slow = new double[]{
-			min.get(VelocityDirection.NORTH) * sign[0],
-			min.get(VelocityDirection.EAST) * sign[1],
-			min.get(VelocityDirection.ROTATION) * sign[2]
-		}; // don't scale below minimum
+		double[] sign = new double[3];
+		for(int i = 0; i < 3; i++){
+
+			VelocityDirection axis = VelocityDirection.values()[i];
+
+			scale[i] /= maxScale; // 0 - 1
+
+			sign[i] = Math.signum(distanceTotal[i]);
+
+			// scale speeds and acceleration/deceleration
+			fast[i] = Math.max(
+				min.get(axis), max.get(axis) * scale[i]
+			) * sign[i];// don't let maximum scale below minimum
+
+			slow[i] = min.get(axis) * sign[i]; // don't scale below minimum
+
+			distanceRemaining[i] = distanceTotal[i];
+			finished[i] = Math.abs(distanceRemaining[0]) == 0.;
+
+		}
+
 		acceleration = new AccelerationLimits(ACCELERATION_G_LIMIT, DECELERATION_G_LIMIT, chassis, scale);
 
 		origin = new Position(chassis.getPosition());
-		distanceRemaining = new double[]{distanceTotal[0], distanceTotal[1], distanceTotal[2]};
-		finished = new boolean[]{
-			Math.abs(distanceRemaining[0]) == 0.,
-			Math.abs(distanceRemaining[1]) == 0.,
-			Math.abs(distanceRemaining[2]) == 0.
-		};
+
 	}
 
 	@Override
 	public void execute() {
 
-		EnumMap<VelocityDirection, Double> vCurrent = chassis.getVelocityCalculated();
-		double[] vNew = {0, 0, 0};
+		EnumMap<VelocityDirection, Double> currentVelocity = chassis.getVelocityCalculated();
+
 		EnumMap<VelocityDirection, Double> distanceTraveled = (new Position(chassis.getPosition())).distanceRelative(
 			origin);
-		boolean[] slow = {false, false, false};
+
+		double[] newVelocity = new double[3];
+
 		for(int i = 0; i < 3; i++) {
-			VelocityDirection direction = VelocityDirection.values()[i];
-			distanceRemaining[i] = distanceTotal[i] - distanceTraveled.get(direction);
-			double distanceToStop = vCurrent.get(direction) * vCurrent.get(direction) /
-									acceleration.get(direction, Type.DECELERATION) /
-									2.;// exact absolute value per physics
+			VelocityDirection axis = VelocityDirection.values()[i];
+
+			distanceRemaining[i] = distanceTotal[i] - distanceTraveled.get(axis);
+
+			double distanceToStop = Math.abs(currentVelocity.get(axis)) /
+					(2 * acceleration.get(axis, Type.DECELERATION));// exact absolute value per physics (should be Vo/2a I think)
+
 			distanceToStop += Math.max(
 				distanceToStop * .05,
-				Math.abs(vCurrent.get(direction)) * .04
+				Math.abs(currentVelocity.get(axis)) * .04
 			); // pad just a bit to avoid overshoot
-			slow[i] = finished[i] ||
-					  Math.abs(distanceRemaining[i]) <= distanceToStop; // slow down within padded stopping distance
-		}
-		boolean s = Math.abs(distanceTotal[2]) > 0 ? slow[2] : (slow[0] && slow[1]);
-		for(int i = 0; i < 3; i++) {
-			vNew[i] = finished[i] ? 0. : s ? this.slow[i] : fast[i];
+
+			boolean useSlow = Math.abs(distanceRemaining[i]) <= distanceToStop; // slow down within padded stopping distance
+
+			if(finished[i]) continue;
+
+			newVelocity[i] = useSlow ? slow[i] : fast[i];
+
 		}
 
-		switch(method) {
-			case 3:
-				chassis.setVelocity(vNew[0], vNew[0], vNew[2], acceleration);
-				break;
-			case 2:
-				chassis.setVelocity(vNew[0], vNew[0], acceleration);
-				break;
-			case 1:
-				chassis.setRotation(vNew[2], acceleration);
-				break;
-		}
+//		boolean s = Math.abs(distanceTotal[2]) > 0 ? useSlow[2] : (useSlow[0] && useSlow[1]);
+
+//		for(int i = 0; i < 3; i++) {
+//			newVelocity[i] = finished[i] ? 0. : s ? this.slow[i] : fast[i];
+//		}
+
+		chassis.setVelocity(newVelocity[0], newVelocity[1], newVelocity[2], acceleration);
+
 	}
 
 	@Override
@@ -138,6 +139,7 @@ public class Move extends CommandBase {
 		for(int i = 0; i < 3; i++) {
 			finished[i] = finished[i] || (Math.signum(distanceRemaining[i]) != Math.signum(distanceTotal[i]));
 		}
-		return Math.abs(distanceTotal[2]) > 0 ? finished[2] : (finished[0] && finished[1]);
+		return finished[0] && finished[1] && finished[2];
+//		return Math.abs(distanceTotal[2]) > 0 ? finished[2] : (finished[0] && finished[1]);
 	}
 }
