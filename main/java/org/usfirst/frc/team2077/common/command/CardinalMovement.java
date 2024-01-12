208eefe35758fbad2dc905b43d3fa81c043aa0cd
SquEdgy <me@dfaris.com>
2024-01-12 13:49:45 -0600

    Move Drive stuff to control

--- main/java/org/usfirst/frc/team2077/common/command/CardinalMovement.java
+++ main/java/org/usfirst/frc/team2077/common/command/CardinalMovement.java
@@ -7,7 +7,7 @@ package org.usfirst.frc.team2077.common.command;
 
 import edu.wpi.first.wpilibj2.command.*;
 import org.usfirst.frc.team2077.common.drivetrain.*;
-import org.usfirst.frc.team2077.common.DriveStick;
+import org.usfirst.frc.team2077.common.control.DriveStick;
 import org.usfirst.frc.team2077.common.HardwareRequirements;
 
 public class CardinalMovement extends Command {
