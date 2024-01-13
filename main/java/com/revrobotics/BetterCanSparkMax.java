a79175eceb55560febdb47e411703a81093e486b
SquEdgy <dafman6@outlook.com>
2024-01-13 13:37:27 -0600

    CanSparkMax override that's useful for us

---
+++ main/java/com/revrobotics/BetterCanSparkMax.java
@@ -0,0 +1,24 @@
+package com.revrobotics;
+
+import com.revrobotics.*;
+
+public class BetterCanSparkMax extends CANSparkMax {
+    /**
+     * Create a new object to control a SPARK MAX motor Controller
+     *
+     * @param deviceId The device ID.
+     * @param type     The motor type connected to the controller. Brushless motor wires must be connected
+     *                 to their matching colors and the hall sensor must be plugged in. Brushed motors must be
+     *                 connected to the Red and Black terminals only.
+     */
+    public BetterCanSparkMax(
+          int deviceId,
+          CANSparkLowLevel.MotorType type
+    ) {
+        super(deviceId, type);
+    }
+
+    public void setTargetVelocity(double velocity) {
+        setpointCommand(velocity, ControlType.kVelocity);
+    }
+}
